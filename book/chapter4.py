245884a9530d359e01c44369db92f82eeb2cf8a4
wencai002 <cai.wenjie002@gmail.com>
2020-03-07 23:28:34 +0100

    20200307

--- book/chapter4.py
+++ book/chapter4.py
@@ -39,4 +39,7 @@ for epoch in range(n_epochs):
        eta = learning_schedule(epoch*m+i)
        theta = theta - eta*gradients
 
-print(theta)
+from sklearn.linear_model import SGDRegressor
+sgd_reg = SGDRegressor(max_iter=1000, tol=1e-3, penalty=None, eta0=0.1)
+sgd_reg.fit(X,y.ravel()) # .ravel makes column to array
+print(sgd_reg.intercept_, sgd_reg.coef_)
