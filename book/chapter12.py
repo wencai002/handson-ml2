ecfefe4f8c8b88cec661097f532ab9a752366ce9
wencai002 <cai.wenjie002@gmail.com>
2020-03-23 00:29:10 +0100

    chapter12 20200323

--- book/chapter12.py
+++ book/chapter12.py
@@ -23,12 +23,27 @@ model = keras.models.Sequential([
     keras.layers.Dense(30, activation="selu", kernel_initializer="lecun_normal",input_shape=input_shape),
     keras.layers.Dense(1)
 ])
-def huber_fn(y_true, y_pred):
-    error = y_true-y_pred
-    is_small_error = tf.abs(error)<1
-    squared_loss = tf.square(error) / 2
-    linear_loss = tf.abs(error)-0.5
-    return tf.where(is_small_error,squared_loss,linear_loss)
+# def huber_fn(y_true, y_pred):
+#     error = y_true-y_pred
+#     is_small_error = tf.abs(error)<1
+#     squared_loss = tf.square(error) / 2
+#     linear_loss = tf.abs(error)-0.5
+#     return tf.where(is_small_error,squared_loss,linear_loss)
 
-model.compile(loss=huber_fn,optimizer="nadam",metrics=["accuracy"])
+from tensorflow.keras.losses import Huber
+huber=Huber()
+model.compile(loss=huber,optimizer="nadam",metrics=["accuracy"])
 history = model.fit(X_train, y_train, epochs=10, validation_data=(X_valid_scaled,y_valid))
+
+def create_huber(threshold=1.0):
+    def huber_fn(y_true, y_pred):
+        error = y_true-y_pred
+        is_small_error = tf.abs(error) < threshold
+        squared_loss = tf.square(error) / 2
+        linear_loss = threshold * tf.abs(error)-threshold**2/2
+        return tf.where(is_small_error, squared_loss, linear_loss)
+    return huber_fn
+
+model.compile(loss=create_huber(2.0), optimizer="nadam")
+
+class
