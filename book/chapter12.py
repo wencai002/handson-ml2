cc8a95e911f769c6b794e16088e0f8b1d80bfaeb
wencai002 <cai.wenjie002@gmail.com>
2020-03-27 23:57:35 +0100

    20200327

--- book/chapter12.py
+++ book/chapter12.py
@@ -44,6 +44,58 @@ def create_huber(threshold=1.0):
         return tf.where(is_small_error, squared_loss, linear_loss)
     return huber_fn
 
+class HuberLoss(keras.losses.Loss):
+    def __init__(self, threshold = 1.0, **kwargs):
+        self.threshold = threshold
+        super().__init__(**kwargs)
+    def call(self, y_true, y_pred):
+        error = y_true - y_pred
+        is_small_error = tf.abs(error) < self.threshold
+        squared_loss = tf.square(error)
+        linear_loss = self.threshold * tf.abs(error) - self.threshold**2/2
+        return tf.where(is_small_error, squared_loss, linear_loss)
+    def get_config(self):
+        base_config = super().get_config()
+        return {**base_config, "threshold": self.threshold}
+
 model.compile(loss=create_huber(2.0), optimizer="nadam")
 
-class
+class HuberMetric(keras.metrics.Metric):
+    def __init__(self, threshold=1.0, **kwargs):
+        super().__init__(**kwargs)
+        self.threshold = threshold
+        self.huber_fn = create_huber(threshold)
+        self.total = self.add_weight("total", initializer="zeros")
+        self.count = self.add_weight("count", initializer="zeros")
+    def update_state(self, y_true, y_pred, sample_weight=None):
+        metric = self.huber_fn(y_true, y_pred)
+        self.total.assign_add(tf.reduce_sum(metric))
+        self.total.assign_add(tf.cast(tf.size(y_true), tf.float32))
+    def result(self):
+        return self.total/self.count
+    def get_config(self):
+        base_config = super().get_config()
+        return {**base_config, "threshold": self.threshold}
+
+class MyDense(keras.layers.layer):
+    def __init__(self, units, activation=None, **kwargs):
+        super().__init__(*kwargs)
+        self.units = units
+        self.activation = keras.activations.get(activation)
+
+    def build(self, batch_input_shape):
+        self.kernel = self.add_weight(
+            name="kernel", shape=[batch_input_shape[-1], self.units],
+            initializer = "glorot_normal")
+        self.bias = self.add_weight(
+            name="bias", shape=[self.units], initializer="zeros")
+        super().build(bacth_input_shape)
+
+    def call(self, X):
+        return self.activation(X @ self.kernel + self.bias)
+
+    def compute_output_shape(self, batch_input_shape):
+        return tf.TensorShape(batch_input_shape.as_lists()[:-1] + [self.units])
+    def get_config(self):
+        base_config = super().get_config()
+        return {**base_config, "units": self.units, "activation": keras.activations.serialize(self.activation)}
